eaf99c0f110bfe271d4446569613d4b56fc02636
HFrodo <egyepong10@gmail.com>
2024-07-31 20:39:17 +0000

    first project files

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
-#if ndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 ssize_t write(int fd, const void *buf, size_t count);
