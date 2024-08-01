d53831a5ef47dd70eaa759dc0d16db5599130b2a
Byte-Master63 <thokozanetek@gmail.com>
2024-07-31 19:09:31 -0700

    task01

--- main.h
+++ main.h
@@ -7,7 +7,6 @@
 int _printf(const char *format, ...);
 char *convert(unsigned long int num, int base, int lowercase);
 char *print_number(int num);
-int str_to_int(const char *str);
 void carry_chars(va_list argums, int *count);
 void carry_strings(va_list argums, int *count);
 void handle_specifier(const char *format, va_list argums, int *count, int *i);
